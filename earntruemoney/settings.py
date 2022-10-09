a6eb810daeeb4ab4855048a3af4bbcd871929bad
samuel aniekan <samuelaniekan680@gmail.com>
2022-10-09 22:18:11 +0100

    production

--- earntruemoney/settings.py
+++ earntruemoney/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production http://127.0.0.1:8000/!
 
+print("DEBUG", env.bool("DEBUG"))
 DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
