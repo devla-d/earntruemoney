3668ef7061d0ebaf9ec17d29ff42bbade526ecf9
samuel aniekan <samuelaniekan680@gmail.com>
2022-10-09 21:28:08 +0100

    production

--- earntruemoney/settings.py
+++ earntruemoney/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 
 STATIC_URL = "assets/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
+STATIC_ROOT = "/home/earngysu/public_html/assets/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
